76997da23b7ccfab4e83d5b04ef7cc0fc83efa3c
Даниил <64002850+Odessey18@users.noreply.github.com>
2022-04-24 20:39:23 +0300

    Initial commit

---
+++ 19.03/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace _19._03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button13 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ALLNAMES";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Василий",
+            "Петр",
+            "Роман"});
+            this.listBox1.Location = new System.Drawing.Point(12, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Васильев",
+            "Петров",
+            "Романов"});
+            this.listBox2.Location = new System.Drawing.Point(167, 29);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 2;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Items.AddRange(new object[] {
+            "Васильевич",
+            "Петрович",
+            "Романович"});
+            this.listBox3.Location = new System.Drawing.Point(323, 29);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 95);
+            this.listBox3.TabIndex = 3;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(167, 195);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(288, 95);
+            this.listBox4.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(323, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 24);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(273, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 24);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(429, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(26, 24);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ИМЯ";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(168, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "ФАМИЛИЯ";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "RANDOM";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(16, 253);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "CLEAR";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(118, 155);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(26, 24);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "-";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(272, 155);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(26, 24);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "-";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(428, 154);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(26, 24);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "-";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(13, 157);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(99, 27);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "ОБЗОР";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(167, 154);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(99, 27);
+            this.button11.TabIndex = 19;
+            this.button11.Text = "ОБЗОР";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(323, 152);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(99, 27);
+            this.button12.TabIndex = 20;
+            this.button12.Text = "ОБЗОР";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(16, 282);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 21;
+            this.button13.Text = "SAVE";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 311);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "NameRandomaizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ 19.03/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19._03
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> names = new List<string>();
+        List<string> randomlist = new List<string>();
+        Random rnd = new Random();
+        string a = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            names.Clear();
+            listBox4.Items.Clear();
+            for (int x = 0; x < listBox1.Items.Count; x++)
+            {
+                for (int y = 0; y < listBox2.Items.Count; y++)
+                {
+                    for (int z = 0; z < listBox3.Items.Count; z++)
+                    {
+                        names.Add(listBox2.Items[y].ToString() + " " + listBox1.Items[x].ToString() + " " + listBox3.Items[z].ToString());
+                        randomlist.Add(listBox2.Items[y].ToString() + " " + listBox1.Items[x].ToString() + " " + listBox3.Items[z].ToString());
+
+                    }
+                }
+            }
+            for (int i = 0; i < names.Count; i++)
+            {
+                listBox4.Items.Add(names[i]);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = " ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox2.Text);
+            textBox2.Text = " ";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Add(textBox3.Text);
+            textBox3.Text = " ";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox4.Items.Clear();
+            int index = rnd.Next(0, randomlist.Count);
+            a = randomlist[index];
+            listBox4.Items.Add(a);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox4.Items.Clear();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            catch { }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+            }
+            catch { }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox3.Items.RemoveAt(listBox3.SelectedIndex);
+            }
+            catch { }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.Multiselect = true;
+            OpenFileDialog1.Filter = "TXT Files|*.txt";
+            OpenFileDialog1.Title = "Select a TXT File";
+            if (OpenFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                listBox1.Items.AddRange(OpenFileDialog1.FileNames);
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.Multiselect = true;
+            OpenFileDialog1.Filter = "TXT Files|*.txt";
+            OpenFileDialog1.Title = "Select a TXT File";
+            if (OpenFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                listBox2.Items.AddRange(OpenFileDialog1.FileNames);
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.Multiselect = true;
+            OpenFileDialog1.Filter = "TXT Files|*.txt";
+            OpenFileDialog1.Title = "Select a TXT File";
+            if (OpenFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                listBox3.Items.AddRange(OpenFileDialog1.FileNames);
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.StreamWriter sw = new System.IO.StreamWriter(saveFileDialog1.FileName);
+
+                    foreach (object item in listBox4.Items)
+
+                        sw.WriteLine(item.ToString());
+
+                    sw.Close();
+                };
+            }
+        }
+    }
+  
+}
